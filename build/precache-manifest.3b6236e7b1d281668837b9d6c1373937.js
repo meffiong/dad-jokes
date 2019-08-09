48fdc8e07e01fcec52d2fafb7e2ace83e728ad18
Mandela Effah Effiong <FBN@Mandelas-MacBook-Pro.local>
2019-08-09 13:29:28 -0500

    Build Folder

---
+++ build/precache-manifest.3b6236e7b1d281668837b9d6c1373937.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd93e03463935cbf7bc8824b8c49cd7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45ee53ca3d18ee1bf6fa",
+    "url": "/static/css/main.23583f94.chunk.css"
+  },
+  {
+    "revision": "efbf41478ac2aa16b538",
+    "url": "/static/js/2.eb10d505.chunk.js"
+  },
+  {
+    "revision": "45ee53ca3d18ee1bf6fa",
+    "url": "/static/js/main.053301d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
